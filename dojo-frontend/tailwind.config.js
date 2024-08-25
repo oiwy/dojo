064e24d5d27d13c302257a9094b314e60c80926d
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-08-25 16:19:09 +0700

    feat: update tailwind.config

--- dojo-frontend/tailwind.config.js
+++ dojo-frontend/tailwind.config.js
@@ -12,7 +12,27 @@ export default {
         "2xl": "6rem",
       },
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        shake: {
+          "10%, 90%": {
+            transform: "translate3d(-1px, 0, 0)",
+          },
+          "20%, 80%": {
+            transform: "translate3d(6px, 0, 0)",
+          },
+          "30%, 50%, 70%": {
+            transform: "translate3d(-4px, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        shake: "shake 1s ease-in-out infinite",
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dracula", "light"],
+  },
 };
