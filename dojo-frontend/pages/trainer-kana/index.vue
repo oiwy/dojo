cf99db99feb4af2ea1af41842a0f520fcd179a7d
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-09-04 02:46:32 +0700

    feat: update trainer-kana

--- dojo-frontend/pages/trainer-kana/index.vue
+++ dojo-frontend/pages/trainer-kana/index.vue
@@ -1,16 +1,8 @@
 <template>
   <section>
-    <div class="container py-8 flex flex-col items-center justify-center gap-4">
+    <div class="container flex flex-col items-center justify-center gap-4 py-8">
       <div class="flex flex-col gap-2">
         <TrainerKana />
-        <!-- <p class="w-1/2">
-          Kana is a writing system used in the Japanese language, which consists
-          of two syllabaries: Katakana and Hiragana. Katakana is used to write
-          foreign words, loanwords, emphasis, and onomatopoeic words, while
-          Hiragana is used to write native Japanese words and grammatical
-          elements. Studying Kana is an essential part of learning Japanese, as
-          it provides the foundation for reading and writing Japanese.
-        </p> -->
       </div>
     </div>
   </section>
