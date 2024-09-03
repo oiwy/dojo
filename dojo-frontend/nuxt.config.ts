54b35d0530d47f59b0bd1e5f8ce9e1de428397b1
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-09-04 03:00:10 +0700

    feat: update nuxt config

--- dojo-frontend/nuxt.config.ts
+++ dojo-frontend/nuxt.config.ts
@@ -7,5 +7,28 @@ export default defineNuxtConfig({
     },
   },
   devtools: { enabled: true },
+  ssr: true,
   compatibilityDate: "2024-08-28",
+  app: {
+    head: {
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "title",
+          content: "Dojo - Ваше место для мастерства языка",
+        },
+        {
+          name: "description",
+          content:
+            "Dojo — Ваше место для мастерства языка. Овладейте японским алфавитом (Катаканой и Хираганой), грамотой и навыками чтения — все в одном месте.",
+        },
+      ],
+    },
+  },
 });
